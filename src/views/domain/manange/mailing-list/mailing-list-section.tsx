0dccbcff8202942f5168049d16871d0309038a65
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-09-07 11:29:41 +0530

    chore: dynamic list owner functionality added

--- src/views/domain/manange/mailing-list/mailing-list-section.tsx
+++ src/views/domain/manange/mailing-list/mailing-list-section.tsx
@@ -12,17 +12,23 @@ import {
 	Switch,
 	Icon,
 	Table,
-	Padding
+	Padding,
+	Button,
+	Dropdown,
+	SnackbarManagerContext
 } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
+import { debounce, sortedUniq, uniq } from 'lodash';
 import { MailingListContext } from './mailinglist-context';
 import ListRow from '../../../list/list-row';
 import { searchDirectory } from '../../../../services/search-directory-service';
-import { isValidLdapQuery } from '../../../utility/utils';
+import { getAllEmailFromString, isValidEmail, isValidLdapQuery } from '../../../utility/utils';
+import { searchGal } from '../../../../services/search-gal-service';
 
 const MailingListSection: FC<any> = () => {
 	const { t } = useTranslation();
 	const context = useContext(MailingListContext);
+	const createSnackbar: any = useContext(SnackbarManagerContext);
 	const [domainList, setDomainList] = useState([]);
 	const [isValidQuery, setIsValidQuery] = useState<boolean>(true);
 	const { mailingListDetail, setMailingListDetail } = context;
@@ -32,6 +38,49 @@ const MailingListSection: FC<any> = () => {
 	const [dynamicListMemberRows, setDynamicListMemberRows] = useState<Array<any>>([]);
 	const [isShowLdapQueryMessage, setIsShowLdapQueryMessage] = useState<boolean>(false);
 	const [ldapQueryErrorMessage, setLdapQueryErrorMessage] = useState<string>('');
+	const [searchMemberResult, setSearchMemberResult] = useState<Array<any>>([]);
+	const [member, setMember] = useState<string>('');
+	const [ownersList, setOwnersList] = useState<Array<any>>(
+		mailingListDetail?.owners ? mailingListDetail?.owners : []
+	);
+	const [selectedDistributionListOwner, setSelectedDistributionListOwner] = useState<Array<any>>(
+		[]
+	);
+	const [ownerTableRows, setOwnerTableRows] = useState<Array<any>>([]);
+	const ownerHeaders: any[] = useMemo(
+		() => [
+			{
+				id: 'members',
+				label: t('label.accounts_that_are_owners', 'Accounts that are owners'),
+				width: '100%',
+				bold: true
+			}
+		],
+		[t]
+	);
+	useEffect(() => {
+		if (ownersList && ownersList.length > 0) {
+			setMailingListDetail((prev: any) => ({
+				...prev,
+				owners: ownersList
+			}));
+			const allRows = ownersList.map((item: any) => ({
+				id: item,
+				columns: [
+					<Text size="medium" weight="bold" key={item?.id} color="#828282">
+						{item}
+					</Text>
+				]
+			}));
+			setOwnerTableRows(allRows);
+		} else {
+			setMailingListDetail((prev: any) => ({
+				...prev,
+				owners: []
+			}));
+			setOwnerTableRows([]);
+		}
+	}, [ownersList, setMailingListDetail]);
 	const changeResourceDetail = useCallback(
 		(e) => {
 			if (e.target.name === 'memberURL') {
@@ -116,6 +165,111 @@ const MailingListSection: FC<any> = () => {
 		}
 	}, [dynamicListMember, setMailingListDetail]);
 
+	const getSearchMemberList = useCallback(
+		(searchKeyword) => {
+			searchGal(searchKeyword).then((data) => {
+				const contactList = data?.cn;
+				if (contactList) {
+					let result: any[] = [];
+					result = contactList.map((item: any): any => ({
+						id: item?.id,
+						name: item?._attrs?.email
+					}));
+					setSearchMemberResult(result);
+					setMailingListDetail((prev: any) => ({
+						...prev,
+						allOwnersList: mailingListDetail?.allOwnersList.concat(contactList)
+					}));
+				} else {
+					setSearchMemberResult([]);
+				}
+			});
+		},
+		[setMailingListDetail, mailingListDetail?.allOwnersList]
+	);
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	const searchMemberCall = useCallback(
+		debounce((mem) => {
+			getSearchMemberList(mem);
+		}, 700),
+		[debounce]
+	);
+	useEffect(() => {
+		if (member !== '') {
+			searchMemberCall(member);
+		}
+	}, [member, searchMemberCall]);
+
+	const items = searchMemberResult.map((item: any, index) => ({
+		id: item?.id,
+		label: item?.name,
+		customComponent: (
+			<Row
+				top="9px"
+				right="large"
+				bottom="9px"
+				left="large"
+				style={{
+					fontFamily: 'roboto',
+					display: 'block',
+					textAlign: 'left',
+					height: 'inherit',
+					padding: '3px',
+					width: 'inherit'
+				}}
+				onClick={(): void => {
+					setMember(item?.name);
+				}}
+			>
+				{item?.name}
+			</Row>
+		)
+	}));
+
+	const onAdd = useCallback((): void => {
+		if (member !== '') {
+			const specialChars = /[ `'"<>,;]/;
+			const allEmails: any[] = specialChars.test(member) ? getAllEmailFromString(member) : [member];
+			if (allEmails !== null && allEmails !== undefined) {
+				const inValidEmailAddress = allEmails.filter((item: any) => !isValidEmail(item));
+				if (inValidEmailAddress && inValidEmailAddress.length > 0) {
+					createSnackbar({
+						key: 'error',
+						type: 'error',
+						label: `${t('label.invalid_email_address', 'Invalid email address')} ${
+							inValidEmailAddress[0]
+						}`,
+						autoHideTimeout: 3000,
+						hideButton: true,
+						replace: true
+					});
+				} else {
+					setMember('');
+					const sortedList = sortedUniq(allEmails);
+					setOwnersList(uniq(ownersList.concat(sortedList)));
+				}
+			} else if (allEmails === undefined) {
+				createSnackbar({
+					key: 'error',
+					type: 'error',
+					label: `${t('label.invalid_email_address', 'Invalid email address')} ${member}`,
+					autoHideTimeout: 3000,
+					hideButton: true,
+					replace: true
+				});
+			}
+		}
+	}, [member, createSnackbar, ownersList, t]);
+
+	const onDeleteFromList = useCallback((): void => {
+		if (selectedDistributionListOwner.length > 0) {
+			const _dlm = ownersList.filter((item: any) => !selectedDistributionListOwner.includes(item));
+			setOwnersList(_dlm);
+			setSelectedDistributionListOwner([]);
+		}
+	}, [ownersList, selectedDistributionListOwner]);
+
 	return (
 		<Container mainAlignment="flex-start">
 			<Container
@@ -193,6 +347,85 @@ const MailingListSection: FC<any> = () => {
 						/>
 					</Container>
 				</ListRow>
+				{mailingListDetail?.dynamic && (
+					<>
+						<ListRow>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="flex-start"
+								orientation="horizontal"
+								padding={{ top: 'medium', bottom: 'medium' }}
+							>
+								<Switch
+									value={mailingListDetail?.zimbraHideInGal}
+									label={t('label.hidden_from_gal', 'Hidden from GAL')}
+									onClick={(): void => {
+										setMailingListDetail((prev: any) => ({
+											...prev,
+											zimbraHideInGal: !mailingListDetail?.zimbraHideInGal
+										}));
+									}}
+								/>
+							</Container>
+						</ListRow>
+						<ListRow>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="flex-start"
+								orientation="horizontal"
+								padding={{ top: 'medium', bottom: 'medium' }}
+							>
+								<Switch
+									value={mailingListDetail?.zimbraMailStatus}
+									label={t('label.can_receive_email', 'Can receive email')}
+									onClick={(): void => {
+										setMailingListDetail((prev: any) => ({
+											...prev,
+											zimbraMailStatus: !mailingListDetail?.zimbraMailStatus
+										}));
+									}}
+								/>
+							</Container>
+						</ListRow>
+						<ListRow>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="flex-start"
+								orientation="horizontal"
+								padding={{ top: 'small', bottom: 'medium' }}
+							>
+								<Input
+									label={t('label.list_url', 'List URL')}
+									backgroundColor="gray5"
+									value={mailingListDetail?.memberURL}
+									size="medium"
+									inputName="memberURL"
+									onChange={changeResourceDetail}
+									hasError={!isValidQuery}
+									CustomIcon={(): any => (
+										<Icon
+											icon="CheckmarkOutline"
+											size="large"
+											color="grey"
+											onClick={getMemberFromLdapQuery}
+										/>
+									)}
+								/>
+							</Container>
+						</ListRow>
+						{isShowLdapQueryMessage && (
+							<Row>
+								<Container mainAlignment="flex-start" crossAlignment="flex-start" width="fill">
+									<Padding>
+										<Text size="extrasmall" weight="regular" color="error">
+											{ldapQueryErrorMessage}
+										</Text>
+									</Padding>
+								</Container>
+							</Row>
+						)}
+					</>
+				)}
 				<ListRow>
 					<Container
 						mainAlignment="flex-start"
@@ -212,46 +445,6 @@ const MailingListSection: FC<any> = () => {
 						/>
 					</Container>
 				</ListRow>
-				{mailingListDetail?.dynamic && (
-					<ListRow>
-						<Container
-							mainAlignment="flex-start"
-							crossAlignment="flex-start"
-							orientation="horizontal"
-							padding={{ top: 'small', bottom: 'medium' }}
-						>
-							<Input
-								label={t('label.list_url', 'List URL')}
-								backgroundColor="gray5"
-								value={mailingListDetail?.memberURL}
-								size="medium"
-								inputName="memberURL"
-								onChange={changeResourceDetail}
-								hasError={!isValidQuery}
-								CustomIcon={(): any => (
-									<Icon
-										icon="CheckmarkOutline"
-										size="large"
-										color="grey"
-										onClick={getMemberFromLdapQuery}
-									/>
-								)}
-							/>
-						</Container>
-					</ListRow>
-				)}
-
-				{isShowLdapQueryMessage && (
-					<Row>
-						<Container mainAlignment="flex-start" crossAlignment="flex-start" width="fill">
-							<Padding>
-								<Text size="extrasmall" weight="regular" color="error">
-									{ldapQueryErrorMessage}
-								</Text>
-							</Padding>
-						</Container>
-					</Row>
-				)}
 
 				{mailingListDetail?.dynamic && (
 					<>
@@ -269,6 +462,89 @@ const MailingListSection: FC<any> = () => {
 						<ListRow>
 							<Container padding={{ top: 'large', bottom: 'large' }}>
 								<Table rows={dynamicListMemberRows} headers={memberHeaders} showCheckbox={false} />
+							</Container>
+						</ListRow>
+
+						<ListRow>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="flex-start"
+								orientation="horizontal"
+								padding={{ top: 'large', right: 'small' }}
+								width="65%"
+							>
+								<Dropdown
+									items={items}
+									placement="bottom-start"
+									maxWidth="300px"
+									disableAutoFocus
+									width="265px"
+									style={{
+										width: '100%'
+									}}
+								>
+									<Input
+										label={t('label.type_an_account_dot', 'Type an account ...')}
+										backgroundColor="gray5"
+										size="medium"
+										value={member}
+										onChange={(e: any): void => {
+											setMember(e.target.value);
+										}}
+									/>
+								</Dropdown>
+							</Container>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="center"
+								orientation="horizontal"
+								width="fit"
+								padding={{ top: 'large', right: 'small' }}
+							>
+								<Button
+									type="outlined"
+									label={t('label.add', 'Add')}
+									color="primary"
+									icon="PlusOutline"
+									iconPlacement="right"
+									height={44}
+									onClick={onAdd}
+									disabled={member === ''}
+								/>
+							</Container>
+							<Container
+								mainAlignment="flex-start"
+								crossAlignment="center"
+								orientation="horizontal"
+								padding={{ top: 'large', right: 'small' }}
+								width="fit"
+							>
+								<Button
+									type="outlined"
+									label={t('label.delete', 'Delete')}
+									color="error"
+									icon="PlusOutline"
+									iconPlacement="right"
+									height={44}
+									onClick={onDeleteFromList}
+									disabled={
+										selectedDistributionListOwner && selectedDistributionListOwner.length === 0
+									}
+								/>
+							</Container>
+						</ListRow>
+
+						<ListRow>
+							<Container padding={{ top: 'large' }}>
+								<Table
+									rows={ownerTableRows}
+									headers={ownerHeaders}
+									showCheckbox={false}
+									selectedRows={selectedDistributionListOwner}
+									onSelectionChange={(selected: any): void =>
+										setSelectedDistributionListOwner(selected)
+									}
+								/>
 							</Container>
 						</ListRow>
 					</>
@@ -290,50 +566,6 @@ const MailingListSection: FC<any> = () => {
 						/>
 					</Container>
 				</ListRow>
-
-				{mailingListDetail?.dynamic && (
-					<ListRow>
-						<Container
-							mainAlignment="flex-start"
-							crossAlignment="flex-start"
-							orientation="horizontal"
-							padding={{ top: 'medium', bottom: 'medium' }}
-						>
-							<Switch
-								value={mailingListDetail?.zimbraHideInGal}
-								label={t('label.hidden_from_gal', 'Hidden from GAL')}
-								onClick={(): void => {
-									setMailingListDetail((prev: any) => ({
-										...prev,
-										zimbraHideInGal: !mailingListDetail?.zimbraHideInGal
-									}));
-								}}
-							/>
-						</Container>
-					</ListRow>
-				)}
-
-				{mailingListDetail?.dynamic && (
-					<ListRow>
-						<Container
-							mainAlignment="flex-start"
-							crossAlignment="flex-start"
-							orientation="horizontal"
-							padding={{ top: 'medium', bottom: 'medium' }}
-						>
-							<Switch
-								value={mailingListDetail?.zimbraMailStatus}
-								label={t('label.can_receive_email', 'Can receive email')}
-								onClick={(): void => {
-									setMailingListDetail((prev: any) => ({
-										...prev,
-										zimbraMailStatus: !mailingListDetail?.zimbraMailStatus
-									}));
-								}}
-							/>
-						</Container>
-					</ListRow>
-				)}
 			</Container>
 		</Container>
 	);
